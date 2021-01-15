b47fa18102b9ac65e2688f54ac58aeab2c2e9641
suhyunbaik <suhyunbaik89@gmail.com>
2021-01-15 04:05:15 +0000

    Deploying to gh-pages from  @ fb80d7dfb247e16bd2460a47f7e67334078e64a4 🚀

--- components/date.js
+++
@@ -1,6 +0,0 @@
-import { parseISO, format, formatDistance, subDays } from 'date-fns'
-
-export default function Date({ dateString }) {
-    const date = parseISO(dateString)
-    return <time dateTime={dateString}>{format(date, 'yyyy-M-d')}</time>
-}
--- next.config.js
+++
@@ -1,7 +0,0 @@
-const debug = process.env.NODE_ENV !== 'production'
-const name = 'test'
-
-module.exports = {
-    basePath: `/${name}`,
-    assetPrefix: `/${name}/`,
-};
